84f9b6d2827215c222b48c026f4fc393a35b8ea6
Shaik Basha16 <shaik.basha16@mphasis.com>
2025-02-03 10:04:05 +0530

    First Commit

---
+++ src/main/java/com/Practice/GitDemoProject/App.java
@@ -0,0 +1,16 @@
+package com.Practice.GitDemoProject;
+
+
+public class App {
+	
+	
+    public static void main( String[] args ){
+        
+    	int a=10,b=20;
+    	int c=a+b;
+    	System.out.println("Addition: "+c);
+    	System.out.println("Demo for Git Accounnt");
+    	
+    	
+    }
+}
